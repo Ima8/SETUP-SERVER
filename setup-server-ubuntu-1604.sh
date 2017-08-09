3e21c94d2592eb5f5c9b6f357f4863a40b4ba0fd
Issaret Prachimutita <imac.monochrome@gmail.com>
2017-08-09 13:35:38 +0700

    Edit

--- setup-server-ubuntu-1604.sh
+++ setup-server-ubuntu-1604.sh
@@ -2,14 +2,10 @@ sudo apt install htop
 sudo apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt-get update
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
-sudo systemctl status docker
 sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
